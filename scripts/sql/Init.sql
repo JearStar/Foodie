b64989680f22eda0376083f1b9c140166a96f3fb
jearstar <jearstar@students.cs.ubc.ca>
2024-11-12 21:40:22 -0800

    FIX SQL SCRIPTS AND ADDED DROP TABLES SCRIPT

--- scripts/sql/Init.sql
+++ scripts/sql/Init.sql
@@ -1,31 +1,126 @@
-drop table User;
-drop table Vote;
-drop table Comment;
-drop table Photo;
-drop table FoodLocation;
-drop table FoodLocationSummary;
-drop table LimitedTimeDish;
-drop table Review;
-drop table ReviewsDish;
-drop table Dish;
+ALTER SESSION SET CURRENT_SCHEMA = ORA_<CWL>;
 
-CREATE TABLE User (
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE ReviewsDish';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE LimitedTimeDish';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE Dish';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE Photo';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE Review';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER TABLE FoodLocation DROP CONSTRAINT fk_foodlocation_reference_summary';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER TABLE Vote DROP CONSTRAINT fk_vote_reference_appuser';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER TABLE Vote DROP CONSTRAINT fk_vote_reference_usercomment';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'ALTER TABLE UserComment DROP CONSTRAINT fk_usercomment_reference_appuser';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE Vote';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE FoodLocationSummary';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE FoodLocation';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE UserComment';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE AppUser';
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
+
+
+CREATE TABLE AppUser (
     UserID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
     Email VARCHAR(50) NOT NULL,
     Password VARCHAR(20) NOT NULL,
     NumReviews INTEGER NOT NULL
 );
 
-CREATE TABLE Vote (
-    VoteID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
-    Value BOOLEAN NOT NULL,
-    UserID RAW(16) DEFAULT SYS_GUID() NOT NULL,
-    CommentID RAW(16) DEFAULT SYS_GUID() NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES User ON DELETE CASCADE,
-    FOREIGN KEY (CommentID) REFERENCES Comment ON DELETE CASCADE
-);
-
-CREATE TABLE Comment (
+CREATE TABLE UserComment (
     CommentID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
     CommentLikes INTEGER NOT NULL,
     Content VARCHAR(1000) NOT NULL,
@@ -34,7 +129,59 @@ CREATE TABLE Comment (
     ParentType VARCHAR(10) NOT NULL,
     UserID RAW(16) DEFAULT SYS_GUID() NOT NULL,
     CHECK (ParentType IN ('Review', 'Comment')),
-    FOREIGN KEY (UserID) REFERENCES User ON DELETE CASCADE
+    CONSTRAINT fk_usercomment_reference_appuser FOREIGN KEY (UserID) REFERENCES AppUser ON DELETE CASCADE
+);
+
+CREATE TABLE FoodLocation (
+    FoodLocationName VARCHAR(50),
+    NumReviews INTEGER NOT NULL,
+    Address VARCHAR(150),
+    City VARCHAR(100) NOT NULL,
+    PostalCode VARCHAR(10),
+    Country VARCHAR(50),
+    Genre VARCHAR(50) NOT NULL,
+    FoodLocationSummaryID RAW(16) DEFAULT SYS_GUID() NOT NULL,
+    PRIMARY KEY (FoodLocationName, Address, Country, PostalCode)
+);
+
+CREATE TABLE Vote (
+    VoteID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
+    Value NUMBER(1) NOT NULL, -- boolean
+    UserID RAW(16) DEFAULT SYS_GUID() NOT NULL,
+    CommentID RAW(16) DEFAULT SYS_GUID() NOT NULL,
+    CONSTRAINT check_boolean CHECK (Value IN (0, 1)),
+    CONSTRAINT fk_vote_reference_appuser FOREIGN KEY (UserID) REFERENCES AppUser ON DELETE CASCADE,
+    CONSTRAINT fk_vote_reference_usercomment FOREIGN KEY (CommentID) REFERENCES UserComment ON DELETE CASCADE
+);
+
+
+CREATE TABLE FoodLocationSummary (
+    SummaryID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
+    AverageRating FLOAT NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    FoodLocationName VARCHAR(50) NOT NULL,
+    Address VARCHAR(150) NOT NULL,
+    PostalCode VARCHAR(10) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
+    FOREIGN KEY (FoodLocationName, Address, PostalCode, Country) REFERENCES FoodLocation ON DELETE CASCADE
+);
+
+ALTER TABLE FoodLocation ADD CONSTRAINT fk_foodlocation_reference_summary FOREIGN KEY (FoodLocationSummaryID) REFERENCES FoodLocationSummary(SummaryID);
+
+CREATE TABLE Review (
+    ReviewID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
+    OverallRating SMALLINT NOT NULL,
+    ServiceRating SMALLINT NOT NULL,
+    WaitTimeRating SMALLINT NOT NULL,
+    DayOfWeekVisited SMALLINT NOT NULL,
+    ReviewTimeStamp TIMESTAMP NOT NULL,
+    FoodLocationName VARCHAR(50) NOT NULL,
+    Address VARCHAR(150) NOT NULL,
+    PostalCode VARCHAR(10) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
+    UserID RAW(16) DEFAULT SYS_GUID() NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES AppUser ON DELETE CASCADE,
+    FOREIGN KEY (FoodLocationName, Address, Country, PostalCode) REFERENCES FoodLocation ON DELETE CASCADE
 );
 
 CREATE TABLE Photo (
@@ -49,28 +196,24 @@ CREATE TABLE Photo (
     FOREIGN KEY (ReviewID) REFERENCES Review ON DELETE CASCADE
 );
 
-CREATE TABLE FoodLocation (
+CREATE TABLE Dish (
+    DishName VARCHAR(50),
+    Price FLOAT NOT NULL,
+    Type VARCHAR(20) NOT NULL,
+    isHalal NUMBER(1) NOT NULL,
+    isGlutenFree NUMBER(1) NOT NULL,
+    isVegetarian NUMBER(1) NOT NULL,
     FoodLocationName VARCHAR(50),
-    NumReviews INTEGER NOT NULL,
     Address VARCHAR(150),
-    City VARCHAR(100) NOT NULL,
     PostalCode VARCHAR(10),
     Country VARCHAR(50),
-    Genre VARCHAR(50) NOT NULL,
-    FoodLocationSummaryID RAW(16) DEFAULT SYS_GUID() NOT NULL,
-    FOREIGN KEY (FoodLocationSummaryID) REFERENCES FoodLocationSummary(SummaryID),
-    PRIMARY KEY (FoodLocationName, Address, Country, PostalCode)
-);
-
-CREATE TABLE FoodLocationSummary (
-    SummaryID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
-    AverageRating FLOAT NOT NULL,
-    Description VARCHAR(255) NOT NULL,
-    FoodLocationName VARCHAR(50) NOT NULL,
-    Address VARCHAR(150) NOT NULL,
-    PostalCode VARCHAR(10) NOT NULL,
-    Country VARCHAR(50) NOT NULL,
-    FOREIGN KEY (FoodLocationName, Address, PostalCode, Country) REFERENCES FoodLocation ON DELETE CASCADE
+    CONSTRAINT check_booleans CHECK (
+        isHalal IN (0, 1) AND
+        isGlutenFree IN (0, 1) AND
+        isVegetarian IN (0, 1)
+        ),
+    FOREIGN KEY (FoodLocationName, Address, Country, PostalCode) REFERENCES FoodLocation ON DELETE CASCADE,
+    PRIMARY KEY (DishName, FoodLocationName, Address, Country, PostalCode)
 );
 
 CREATE TABLE LimitedTimeDish (
@@ -85,21 +228,6 @@ CREATE TABLE LimitedTimeDish (
     PRIMARY KEY (DishName, FoodLocationName, Address, PostalCode, Country)
 );
 
-CREATE TABLE Review (
-    ReviewID RAW(16) DEFAULT SYS_GUID() PRIMARY KEY,
-    OverallRating SMALLINT NOT NULL,
-    ServiceRating SMALLINT NOT NULL,
-    WaitTimeRating SMALLINT NOT NULL,
-    DayOfWeekVisited SMALLINT NOT NULL,
-    ReviewTimeStamp TIMESTAMP NOT NULL,
-    FoodLocationName VARCHAR(50) NOT NULL,
-    Address VARCHAR(150) NOT NULL,
-    PostalCode VARCHAR(10) NOT NULL,
-    Country VARCHAR(50) NOT NULL,
-    UserID RAW(16) DEFAULT SYS_GUID() NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES User ON DELETE CASCADE,
-    FOREIGN KEY (FoodLocationName, Address, Country, PostalCode) REFERENCES FoodLocation ON DELETE CASCADE
-);
 
 CREATE TABLE ReviewsDish (
     ReviewID RAW(16) DEFAULT SYS_GUID(),
@@ -110,23 +238,8 @@ CREATE TABLE ReviewsDish (
     PostalCode VARCHAR(10),
     Country VARCHAR(50),
     FOREIGN KEY (ReviewID) REFERENCES Review ON DELETE CASCADE,
-    FOREIGN KEY (DishName, FoodLocationName, Address, PostalCode, Country) REFERENCES FoodLocation ON DELETE CASCADE,
+    FOREIGN KEY (DishName, FoodLocationName, Address, PostalCode, Country) REFERENCES Dish ON DELETE CASCADE,
     PRIMARY KEY (ReviewID, DishName, FoodLocationName, Address, PostalCode, Country)
-);
-
-CREATE TABLE Dish (
-    DishName VARCHAR(50),
-    Price FLOAT NOT NULL,
-    Type VARCHAR(20) NOT NULL,
-    isHalal BOOLEAN NOT NULL,
-    isGlutenFree BOOLEAN NOT NULL,
-    isVegetarian BOOLEAN NOT NULL,
-    FoodLocationName VARCHAR(50),
-    Address VARCHAR(150),
-    PostalCode VARCHAR(10),
-    Country VARCHAR(50),
-    FOREIGN KEY (FoodLocationName, Address, Country, PostalCode) REFERENCES FoodLocation ON DELETE CASCADE,
-    PRIMARY KEY (DishName, FoodLocationName, Address, Country, PostalCode)
 );
 
 COMMIT;
