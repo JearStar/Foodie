51c84d958d2f4dd472f5e0e3cc8fa8daa81fa0e1
xanderj <xanderj@student.ubc.ca>
2024-11-20 00:34:29 -0800

    added create account page (havent bug tested cus of the uuid package thing)

--- frontend/src/LoginScreen.js
+++ frontend/src/LoginScreen.js
@@ -5,9 +5,12 @@ import {App} from "./App";
 
 function LoginScreen({onLoginSuccess}) {
     const [username, setUsername] = useState('');
+    const [newUsername, setNewUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
     const [isError, setError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
+    const [errorMsg2, setErrorMsg2] = useState('');
     const [createNewAcc, setCreateNewAcc] = useState(false);
 
     const loginAtt = () => {
@@ -32,49 +35,125 @@ function LoginScreen({onLoginSuccess}) {
             .catch(err => {
                 setError(true);
             })
-    }
+    };
 
-    return (
-        <div className="LoginScreen">
-            <div className="LoginHoriz">
+    // START HERE
+    const newAcc = () => {
+        return fetch( "/api/newAcc",
+            {
+                method: 'POST',
+                body: JSON.stringify({"user": newUsername, "pass": newPassword}),
+                headers: {
+                    'content-type':'application/json'
+                }
+            })
+            .then((res) =>res.json())
+            .then((data) => {
+                const result = data["status"];
+                console.log(data["status"]);
+                if (result === "Successful") {
+                    onLoginSuccess();
+                } else {
+                    setErrorMsg2(result);
+                }
+            })
+            .catch(err => {
+                setError(true);
+            })
+    };
+
+    if (!createNewAcc) {
+        return (
+            <div className="LoginScreen">
+                <div className="LoginHoriz">
+                    <p>
+                        Email:
+                    </p>
+                    <textarea className={"Username"}
+                              defaultValue={""}
+                              onChange={e =>
+                                  setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="LoginHoriz">
+                    <p>
+                        Password:
+                    </p>
+                    <textarea className={"Password"}
+                              defaultValue={""}
+                              onChange={e =>
+                                  setPassword(e.target.value)}
+                    />
+                </div>
+                <div className="LoginHoriz">
+                    <p>
+                        <button onClick={() => setCreateNewAcc(true)}>
+                            Create new account
+                        </button>
+                    </p>
+                    <p>
+                        <button onClick={() => {
+                            loginAtt();
+                        }}>
+                            Log in
+                        </button>
+                    </p>
+                </div>
                 <p>
-                    Email:
+                    {errorMsg}
                 </p>
-                <textarea className={"Username"}
-                          defaultValue={""}
-                          onChange={e =>
-                              setUsername(e.target.value)}
-                />
             </div>
-            <div className="LoginHoriz">
+        );
+    } else {
+        return (
+            <div className="LoginScreen">
                 <p>
-                    Password:
+                    New Account Creation Page:
                 </p>
-                <textarea className={"Password"}
-                          defaultValue={""}
-                          onChange={e =>
-                              setPassword(e.target.value)}
-                />
+                <div className="LoginHoriz">
+                    <p>
+                        Email:
+                    </p>
+                    <textarea className={"Username"}
+                              defaultValue={""}
+                              onChange={e =>
+                                  setNewUsername(e.target.value)}
+                    />
+                </div>
+                <div className="LoginHoriz">
+                    <p>
+                        Password:
+                    </p>
+                    <textarea className={"Password"}
+                              defaultValue={""}
+                              onChange={e =>
+                                  setNewPassword(e.target.value)}
+                    />
+                </div>
+                <div className="LoginHoriz">
+                    <p>
+                        <button onClick={() => {
+                            setCreateNewAcc(false);
+                            setNewPassword('');
+                            setNewUsername('');
+                        }}>
+                            Go Back
+                        </button>
+                    </p>
+                    <p>
+                        <button onClick={() => {
+                            newAcc();
+                        }}>
+                            Create account
+                        </button>
+                    </p>
+                </div>
+                <p>
+                    {errorMsg2}
+                </p>
             </div>
-            <div className="LoginHoriz">
-                <p>
-                    <button onClick={() => setCreateNewAcc(true)}>
-                        Create new account
-                    </button>
-                </p>
-                <p>
-                    <button onClick={() => {
-                        loginAtt();
-                    }}>
-                        Log in
-                    </button>
-                </p>
-            </div>
-            <p>
-                {errorMsg}
-            </p>
-        </div>
-    );
+        );
+    }
 }
 
 export default LoginScreen;
