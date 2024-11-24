6fb2c2ef43e3fb4f76ba30971f89168dbb7c5587
jearstar <jearstar@students.cs.ubc.ca>
2024-11-24 00:53:04 -0800

    update sync script to include node modules

--- scripts/mac/sync-build-and-env.sh
+++ scripts/mac/sync-build-and-env.sh
@@ -2,6 +2,7 @@
 
 # Variables
 BUILD_DIR="frontend/build"  # The build folder path in your React project
+NODE_MODULES_DIR="node_modules"
 ENV_FILE=".env"
 REMOTE_USER="<CWL>"  # SSH username
 REMOTE_HOST="remote.students.cs.ubc.ca"  # SSH server address
@@ -10,6 +11,11 @@ REMOTE_PATH="/home/<FIRST_LETTER_OF_YOUR_CWL>/<CWL>/<REPLACE_WITH_PROJECT_DIRECT
 # Check if build directory exists
 if [ ! -d "$BUILD_DIR" ]; then
   echo "Build directory does not exist. Run 'npm run build' first."
+  exit 1
+fi
+
+if [ ! -d "$NODE_MODULES_DIR" ]; then
+  echo "node_modules directory does not exist. Run 'npm install' first."
   exit 1
 fi
 
@@ -22,6 +28,14 @@ fi
 # Upload .env file to the root of the project folder
 echo "Uploading $ENV_FILE to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH..."
 rsync -avz "$ENV_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+
+# Create /frontend/build directory on the remote server if it doesn't exist
+echo "Creating /node_modules directory on the remote server..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir -p $REMOTE_PATH/node_modules"
+
+# Upload the entire build folder to the /frontend folder on the server
+echo "Uploading $NODE_MODULES_DIR to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/"
+rsync -avz "$NODE_MODULES_DIR" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/"
 
 # Create /frontend/build directory on the remote server if it doesn't exist
 echo "Creating /frontend/build directory on the remote server..."
